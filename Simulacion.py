00e337841601a8970220aa26455092b58b7d558d
DalyOmar <134558776+DalyOmar@users.noreply.github.com>
2024-08-24 19:47:49 -0500

    Upgrade

--- Simulacion.py
+++ Simulacion.py
@@ -1,14 +1,8 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
 @app.route('/sumar', methods=['POST'])
 def sumar():
     data = request.json
+    print(data)  # Esto mostrará los datos en la terminal para verificar
     num1 = data.get('num1')
     num2 = data.get('num2')
     resultado = num1 + num2
     return jsonify({'resultado': resultado})
-
-if __name__ == '__main__':
-    app.run(debug=True)
